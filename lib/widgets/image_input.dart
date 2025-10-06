9c44c2813453ccb39943e5b1679fa402df60549c
afonso.araujo <afonso.araujo@supertroco.com.br>
2025-10-06 18:14:28 -0300

    feat: uso de image_picker

--- lib/widgets/image_input.dart
+++ lib/widgets/image_input.dart
@@ -1,4 +1,6 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class ImageInput extends StatefulWidget {
   const ImageInput({super.key});
@@ -8,8 +10,47 @@ class ImageInput extends StatefulWidget {
 }
 
 class _ImageInputState extends State<ImageInput> {
+  File? _selectedImage;
+
+  void _pickImage() async {
+    final picker = ImagePicker();
+
+    final pickedImage = await picker.pickImage(
+      source: ImageSource.camera,
+      imageQuality: 50,
+      maxWidth: 150,
+    );
+
+    if (pickedImage != null) {
+      setState(() {
+        _selectedImage = File(pickedImage.path);
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+    Widget content = TextButton.icon(
+        icon: const Icon(Icons.camera),
+        label: const Text('Adicionar Imagem'),
+        style: TextButton.styleFrom(
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+        ),
+        onPressed: _pickImage,
+      );
+
+    if (_selectedImage != null) {
+      content = GestureDetector(
+        onTap: _pickImage,
+        child: Image.file(
+          _selectedImage!,
+          fit: BoxFit.cover,
+          width: double.infinity,
+          height: double.infinity,
+        ),
+      );
+    }
+
     return Container(
       height: 250,
       width: double.infinity,
@@ -17,14 +58,7 @@ class _ImageInputState extends State<ImageInput> {
         border: Border.all(width: 1, color: Colors.grey),
         borderRadius: BorderRadius.circular(8),
       ),
-      child: TextButton.icon(
-        icon: const Icon(Icons.camera),
-        label: const Text('Adicionar Imagem'),
-        onPressed: () {},
-        style: TextButton.styleFrom(
-          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
-        ),
-      ),
+      child: content,
     );
   }
 }
