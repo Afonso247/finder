d7c400dd5ac34c4f7c9d705b981c950c93608400
afonso.araujo <afonso.araujo@supertroco.com.br>
2025-10-07 19:19:44 -0300

    feat: preparando para salvar dados localmente

--- lib/providers/user_places.dart
+++ lib/providers/user_places.dart
@@ -1,5 +1,8 @@
 import 'dart:io';
+
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:path_provider/path_provider.dart' as syspaths;
+import 'package:path/path.dart' as path;
 
 import 'package:favorite_places/models/place.dart';
 
@@ -9,8 +12,12 @@ class UserPlacesNotifier extends Notifier<List<Place>> {
   @override
   List<Place> build() => [];
 
-  void addPlace(String title, File image, PlaceLocation location) {
-    final place = Place(title: title, image: image, location: location);
+  void addPlace(String title, File image, PlaceLocation location) async {
+    final appDir = await syspaths.getApplicationDocumentsDirectory();
+    final fileName = path.basename(image.path);
+    final copiedImage = await image.copy('${appDir.path}/$fileName');
+
+    final place = Place(title: title, image: copiedImage, location: location);
     state = [place, ...state];
   }
 }
